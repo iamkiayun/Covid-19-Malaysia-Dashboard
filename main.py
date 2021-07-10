a88f57bfb9059cb05faa031d697769e895f5c23a
kiayun <kiayun.0218@outlook.com>
2021-07-11 02:47:16 +0800

    updated card section

--- main.py
+++ main.py
@@ -115,8 +115,10 @@ update_date = open('update_datetime.txt', 'r')
 st.text(f'{update_date.read()}')
 st.markdown("""
 This app performs simple visualization of covid-19 status in Malaysia
-* **Data source:** [KKM's daily report](https://t.me/cprckkm)
+* **Data source:** 1. [KKM's daily report](https://t.me/cprckkm) 2. [Malaysia's National Covid-19 Immunisation Programme](https://github.com/CITF-Malaysia/citf-public)
 """)
+st.text("")
+st.text("")
 
 states = ['Sabah', 'Selangor', 'Kuala Lumpur', 'Negeri Sembilan', 'Kedah', 'Pulau Pinang', 'labuan', 'Sarawak', 'Johor', 'Perak', 'Pahang', 'Melaka', 'Terengganu', 'Kelantan', 'Putrajaya', 'Perlis']
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -129,33 +131,60 @@ state_select = st.sidebar.selectbox('State', states)
 status_select = st.sidebar.radio('Covid-19 patient status', patient_status)
 # selected_state =
 
-new_cases, new_death, total_case, total_Icu,  = st.beta_columns(4)
+new_cases, new_death, total_Icu, Discharge = st.beta_columns(4)
 with new_cases:
-    st.markdown('**New Cases**')
+    st.markdown(f"<h4 style='line-height: 6px; text-align: left; vertical-align: center'>New Cases</h4>",unsafe_allow_html=True)
+    # st.markdown('**New Cases**')
     new_case_no = chartdata_df['newCase'].iloc[0]
     total_test_conducted = chartdata_df['newTest'].iloc[0]
     positive_rate = chartdata_df['Positivity rate'].iloc[0]
-    st.markdown(f"<h3 style='text-align: left;'>{new_case_no}</h3>", unsafe_allow_html=True)
-    st.markdown(f"<h5 style='text-align: left;'>test: {int(total_test_conducted)}</h5>", unsafe_allow_html=True)
+    totalCase = chartdata_df['totalCase'].iloc[0]
+    newDeath = chartdata_df['newDeath'].iloc[0]
+    totalDeath = chartdata_df['totalDeath'].iloc[0]
+    # st.markdown("<h4 style='line-height: 10px; text-align: left; vertical-align: center'>num.toLocalString</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='line-height: 30px; text-align: left; vertical-align: center'>{new_case_no}</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='line-height: 10px; text-align: left; vertical-align: center'>total cases: {totalCase}</h5>",unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: left;'>new test: {int(total_test_conducted)}</h5>", unsafe_allow_html=True)
     st.markdown(f"<h5 style='text-align: left;'>+ve rate: {positive_rate}%</h5>", unsafe_allow_html=True)
-
+    st.text("")
+    st.text("")
+    st.text("")
 
 with new_death:
-    st.markdown('**New Deaths**')
+    st.markdown(f"<h4 style='line-height: 6px; text-align: left; vertical-align: center'>New Deaths</h4>",unsafe_allow_html=True)
+    st.markdown(f"<h4 style='line-height: 30px; text-align: left; vertical-align: center'>{newDeath}</h4>",unsafe_allow_html=True)
+    st.markdown(f"<h5 style='line-height: 10px; text-align: left; vertical-align: center'>total deaths: {totalDeath}</h5>",unsafe_allow_html=True)
 
-with total_case:
-    st.markdown('**Total Cases**')
 
 with total_Icu:
-    st.markdown('**Current ICU**')
+    chartdata_df['Positivity rate'] = chartdata_df['Positivity rate'].map('{:,.2f}'.format)
+    column_list = chartdata_df.drop(['date', 'Positivity rate'], axis=1).columns.tolist()
+    chartdata_df[column_list] = chartdata_df[column_list].fillna(0).astype(dtype=int)
+    chartdata_df.style.format('{:,}', subset=column_list)
+    totalIcu = chartdata_df['totalIcu'].iloc[0]
+    totalIntubated = chartdata_df['intubated'].iloc[0]
+    newIcu = chartdata_df['newIcu'].iloc[0]
+    newDischarged = chartdata_df['newDischarged'].iloc[0]
+    totalDischarged = chartdata_df['totalDischarged'].iloc[0]
+    cure_rate = round(chartdata_df['totalDischarged'].iloc[0]/ chartdata_df['totalCase'].iloc[0]*100,2)
+    st.markdown(f"<h4 style='line-height: 6px; text-align: left; vertical-align: center'>New ICU</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='line-height: 30px; text-align: left; vertical-align: center'>{newIcu}</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='line-height: 0px; text-align: left; vertical-align: center'>current Icu: {totalIcu}</h5>",unsafe_allow_html=True)
+    st.markdown(f"<h5 style='line-height: 0px; text-align: left; vertical-align: center'>intubated: {totalIntubated}</h5>", unsafe_allow_html=True)
 
+with Discharge:
+    st.markdown(f"<h4 style='line-height: 6px; text-align: left; vertical-align: center'>New Discharged</h4>",unsafe_allow_html=True)
+    st.markdown(f"<h4 style='line-height: 30px; text-align: left; vertical-align: center'>{newDischarged}</h4>",unsafe_allow_html=True)
+    st.markdown(f"<h5 style='line-height: 0px; text-align: left; vertical-align: center'>total discharged: {totalDischarged}</h5>",unsafe_allow_html=True)
+    st.markdown(
+        f"<h5 style='line-height: 0px; text-align: left; vertical-align: center'>cure rate: {cure_rate}%</h5>",unsafe_allow_html=True)
 
 # st.write(chartdata_df)
-chartdata_df['Positivity rate'] = chartdata_df['Positivity rate'] .map('{:,.2f}'.format)
-column_list = chartdata_df.drop(['date','Positivity rate'], axis=1).columns.tolist()
-chartdata_df[column_list] = chartdata_df[column_list].fillna(0).astype(dtype=int)
-s = chartdata_df.style.format('{:,}', subset=column_list)
-st.dataframe(s)
+# chartdata_df['Positivity rate'] = chartdata_df['Positivity rate'] .map('{:,.2f}'.format)
+# column_list = chartdata_df.drop(['date','Positivity rate'], axis=1).columns.tolist()
+# chartdata_df[column_list] = chartdata_df[column_list].fillna(0).astype(dtype=int)
+# s = chartdata_df.style.format('{:,}', subset=column_list)
+st.dataframe(chartdata_df.style.format('{:,}', subset=column_list))
 
 
 #cummulative confirmed cases
@@ -164,8 +193,17 @@ graph = px.bar(chartdata_df2, x='date', y='totalCase',
                    'date': '',
                    'totalCase':''
                },
-               title='Confirmed cases')
+               title='Cumulative confirmed cases')
 st.plotly_chart(graph)
+
+#daily confirmed cases
+graph_daily = px.bar(chartdata_df2, x='date', y='newCase',
+               labels={
+                   'date': '',
+                   'newCase':''
+               },
+               title='Daily confirmed cases')
+st.plotly_chart(graph_daily)
 
 # positive rate
 positive_rate_daily = px.bar(chartdata_df2, x='date', y='Positivity rate',
